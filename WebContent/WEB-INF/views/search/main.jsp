ae3d2fb251facf1cc22fb9e8f87f3eb298414876
admin <admin@192.168.10.26>
2017-03-21 15:48:54 +0900

    재민

--- WebContent/WEB-INF/views/search/main.jsp
+++ WebContent/WEB-INF/views/search/main.jsp
@@ -1,12 +1,38 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
 
-</body>
-</html>
+<style>
+#tags {
+	
+}
+
+#location-list {
+	
+}
+
+#name-list {
+	
+}
+</style>
+
+<div id="tags">
+	<div class="row">
+		<c:forEach items="${tagsList }" var="item">
+			<form action="/search/tag">
+				<button class="submit" name="selectedTag">${item }</button>
+			</form>
+		</c:forEach>
+	</div>
+	<div class="row"></div>
+</div>
+
+<div id="location-list">
+	
+</div>
+
+<div id="name-list">
+	
+</div>
+
--- src/model/LocationDao.java
+++ src/model/LocationDao.java
@@ -16,84 +16,93 @@ import org.springframework.stereotype.Service;
 public class LocationDao {
 	public Map googleLocation(String keyword){
 		Map innerMap = new HashMap();
-		keyword = keyword.replaceAll(" ", "+");
-		String target = "https://maps.googleapis.com/maps/api/geocode/json?address=" + keyword
-				+ "&key=AIzaSyBS-83LLE8F1nvtVtsy1Adu-j4LeS9qAQg&language=ko";
-		// 요청보내는 방법은 꽤 많다.
-		// System.out.println(target);
-		URL url = null;
-		System.out.println(target);
-		try{
-			url = new URL(target);
-		} catch(IOException e1 ){
-			e1.printStackTrace();
-			innerMap.put("lng", 0);
-			innerMap.put("lat", 0);
-		}
-		
+		String [] keyArr = keyword.split("\\s");
+		Loof:
+		for(String key : keyArr){
+			key += "+맛집";
+			String target = "https://maps.googleapis.com/maps/api/geocode/json?address=" + key
+					+ "&key=AIzaSyBS-83LLE8F1nvtVtsy1Adu-j4LeS9qAQg&language=ko";
+			URL url = null;
+			System.out.println(target);
+			try{
+				url = new URL(target);
+			} catch(IOException e1 ){
+				e1.printStackTrace();
+				continue Loof;
+			}
+			
 
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new InputStreamReader(url.openStream()));
-		} catch (IOException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-			innerMap.put("lng", 0);
-			innerMap.put("lat", 0);			
-		}
-		String outstr = "";
-		while (true) {
-			String str = null;
+			BufferedReader br = null;
 			try {
-				str = br.readLine();
-			} catch (IOException e1) {
+				br = new BufferedReader(new InputStreamReader(url.openStream()));
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+			String outstr = "";
+			while (true) {
+				String str = null;
+				try {
+					str = br.readLine();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					innerMap.put("lng", 0);
+					innerMap.put("lat", 0);
+				}
+				if (str == null)
+					break;
+				// out.println(str);
+				outstr += str;
+			}
+
+			try {
+
+				JSONParser jsonParser = new JSONParser();
+
+				// JSON데이터를 넣어 JSON Object 로 만들어 준다.
+				JSONObject jsonObject = (JSONObject) jsonParser.parse(outstr);
+				JSONArray resultArray = (JSONArray) jsonObject.get("results");
+				if (resultArray.size() != 0) {
+					
+					JSONObject resultObject = (JSONObject) jsonParser
+							.parse(resultArray.get(0).toString());
+					JSONArray addressArray = (JSONArray) resultObject.get("address_components");
+					System.out.println(addressArray.toJSONString());
+					for(int i =0; i<addressArray.size(); i++){
+						if(((JSONObject)addressArray.get(i)).get("short_name").toString().equals("KR")){
+							JSONObject geoMetryObject = (JSONObject) resultObject.get("geometry");
+							JSONObject locationObject = (JSONObject) geoMetryObject.get("location");
+							
+							Double lng = (Double) locationObject.get("lng");
+							Double lat = (Double) locationObject.get("lat");
+							
+							// location의 배열을 추출
+							innerMap.put("lng", lng);
+							innerMap.put("lat", lat);
+							System.out.println("google Location innerMap : "+innerMap.toString());
+							break Loof;
+						}
+					}
+					
+					
+				} else {
+					innerMap.put("lng", 0);
+					innerMap.put("lat", 0);
+				}
+
+			} catch (ParseException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 				innerMap.put("lng", 0);
 				innerMap.put("lat", 0);
 			}
-			if (str == null)
-				break;
-			// out.println(str);
-			outstr += str;
+			System.out.println("google Location innerMap : "+innerMap.toString());
+
 		}
 
-		try {
-
-			JSONParser jsonParser = new JSONParser();
-
-			// JSON데이터를 넣어 JSON Object 로 만들어 준다.
-			JSONObject jsonObject = (JSONObject) jsonParser.parse(outstr);
-			JSONArray resultArray = (JSONArray) jsonObject.get("results");
-			if (resultArray.size() != 0) {
-				JSONObject resultObject = (JSONObject) jsonParser
-						.parse(resultArray.get(0).toString());
-				// System.out.println(resultObject.toString());
-				JSONObject geoMetryObject = (JSONObject) resultObject.get("geometry");
-				// System.out.println(geoMetryObject.toString());
-				JSONObject locationObject = (JSONObject) geoMetryObject.get("location");
-				// System.out.println(locationObject.toString());
-				// System.out.println("lat ==
-				// "+latObject.toString());
-				Double lng = (Double) locationObject.get("lng");
-				Double lat = (Double) locationObject.get("lat");
-				// System.out.println("lng ==
-				// "+lngObject.toString());
-				// location의 배열을 추출
-				innerMap.put("lng", lng);
-				innerMap.put("lat", lat);
-			} else {
-				innerMap.put("lng", 0);
-				innerMap.put("lat", 0);
-			}
-
-		} catch (ParseException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			innerMap.put("lng", 0);
-			innerMap.put("lat", 0);
-		}
-		System.out.println("google Location innerMap : "+innerMap.toString());
+		// 요청보내는 방법은 꽤 많다.
+		// System.out.println(target);
 		return innerMap;
 	}
 }
